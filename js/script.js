83dac09434c528b8545ff6c62a46a1e112a09a22
wlademirAntonio <tonymartinsjunior@gmail.com>
2024-04-12 21:48:37 -0300

    a

--- js/script.js
+++ js/script.js
@@ -5,6 +5,16 @@ const insta = document.querySelector(".insta")
 const chat = document.querySelector(".chat")
 const chatForm = document.querySelector(".chat__form")
 const chatInput = document.querySelector(".chat__input")
+const chatMessages = document.querySelector(".chat__messages")
+
+const createMessageSelfElement = (content) => {
+    const div = document.createElement("div")
+
+    div.classList.add("message--self")
+    div.innerHTML = content
+
+    return div
+}
 
 const handleSubmit = (event) => {
     event.preventDefault()
@@ -14,4 +24,22 @@ const handleSubmit = (event) => {
     insta.style.display = "none"
 }
 
+const sendMessage = (event) => {
+    event.preventDefault()
+
+    const message = chatInput.value
+
+    chatInput.value = ""
+
+    const element = createMessageSelfElement(message)
+
+    if (message != "") {
+
+        chatMessages.appendChild(element)
+
+    }
+
+}
+
 entrarForm.addEventListener("submit", handleSubmit)
+chatForm.addEventListener("submit", sendMessage)
